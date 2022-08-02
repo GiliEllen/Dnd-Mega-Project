0d812eeb59da01f78258db0b682cd4832a2cd5c9
Gili <gilimena95@gmail.com>
2022-08-02 19:26:50 +0300

    Loot Dm Css

--- Main/public/scripts/dist/helpers.js
+++ Main/public/scripts/dist/helpers.js
@@ -3,18 +3,36 @@ var createAndSend = document.querySelector('#createAndSend');
 var newHandoutForm = document.querySelector('#newHandoutForm');
 var chooseAndSend = document.querySelector('#chooseAndSend');
 var existingHandoutsForm = document.querySelector('#existingHandoutsForm');
+var createAndSendLoot = document.querySelector('#createAndSendLoot');
+var newLootForm = document.querySelector('#newLootForm');
+var chooseAndSendLoot = document.querySelector('#chooseAndSendLoot');
+var existingLootForm = document.querySelector('#existingLootForm');
 var session = document.querySelector('.session');
-createAndSend.addEventListener('click', function (event) {
-    createAndSend.style.height = '500px';
-    newHandoutForm.style.display = 'flex';
-    newHandoutForm.style.visibility = 'visible';
-    newHandoutForm.style.opacity = '1';
-    console.log("hi");
-});
-chooseAndSend.addEventListener('click', function (event) {
-    chooseAndSend.style.height = 'fit-content';
-    existingHandoutsForm.style.display = 'flex';
-    existingHandoutsForm.style.visibility = 'visible';
-    existingHandoutsForm.style.opacity = '1';
-    console.log("hi");
-});
+if (createAndSend) {
+    createAndSend.addEventListener('click', function (event) {
+        createAndSend.style.height = '500px';
+        newHandoutForm.style.display = 'flex';
+        newHandoutForm.style.visibility = 'visible';
+        newHandoutForm.style.opacity = '1';
+    });
+    chooseAndSend.addEventListener('click', function (event) {
+        chooseAndSend.style.height = 'fit-content';
+        existingHandoutsForm.style.display = 'flex';
+        existingHandoutsForm.style.visibility = 'visible';
+        existingHandoutsForm.style.opacity = '1';
+    });
+}
+if (createAndSendLoot) {
+    createAndSendLoot.addEventListener('click', function (event) {
+        createAndSendLoot.style.height = '500px';
+        newLootForm.style.display = 'flex';
+        newLootForm.style.visibility = 'visible';
+        newLootForm.style.opacity = '1';
+    });
+    chooseAndSendLoot.addEventListener('click', function (event) {
+        chooseAndSendLoot.style.height = 'fit-content';
+        existingLootForm.style.display = 'flex';
+        existingLootForm.style.visibility = 'visible';
+        existingLootForm.style.opacity = '1';
+    });
+}
--- Main/public/scripts/helpers.ts
+++ Main/public/scripts/helpers.ts
@@ -4,23 +4,42 @@ const createAndSend = document.querySelector('#createAndSend') as HTMLDivElement
 const newHandoutForm = document.querySelector('#newHandoutForm') as HTMLFormElement;
 
 const chooseAndSend = document.querySelector('#chooseAndSend') as HTMLDivElement;
-const existingHandoutsForm = document.querySelector('#existingHandoutsForm')  as HTMLFormElement
+const existingHandoutsForm = document.querySelector('#existingHandoutsForm') as HTMLFormElement;
 
-const session = document. querySelector('.session') as HTMLDivElement;
+const createAndSendLoot = document.querySelector('#createAndSendLoot') as HTMLDivElement;
+const newLootForm = document.querySelector('#newLootForm') as HTMLFormElement;
 
-createAndSend.addEventListener('click', (event) => {
-	createAndSend.style.height = '500px';
-	newHandoutForm.style.display = 'flex';
-	newHandoutForm.style.visibility = 'visible';
-	newHandoutForm.style.opacity = '1';
+const chooseAndSendLoot = document.querySelector('#chooseAndSendLoot') as HTMLDivElement;
+const existingLootForm = document.querySelector('#existingLootForm') as HTMLFormElement;
 
-	console.log(`hi`);
-});
-chooseAndSend.addEventListener('click', (event) => {
-	chooseAndSend.style.height = 'fit-content';
-	existingHandoutsForm.style.display = 'flex';
-	existingHandoutsForm.style.visibility = 'visible';
-	existingHandoutsForm.style.opacity = '1';
+const session = document.querySelector('.session') as HTMLDivElement;
 
-	console.log(`hi`);
-});
+if (createAndSend) {
+	createAndSend.addEventListener('click', (event) => {
+		createAndSend.style.height = '500px';
+		newHandoutForm.style.display = 'flex';
+		newHandoutForm.style.visibility = 'visible';
+		newHandoutForm.style.opacity = '1';
+	});
+	chooseAndSend.addEventListener('click', (event) => {
+		chooseAndSend.style.height = 'fit-content';
+		existingHandoutsForm.style.display = 'flex';
+		existingHandoutsForm.style.visibility = 'visible';
+		existingHandoutsForm.style.opacity = '1';
+	});
+}
+
+if (createAndSendLoot) {
+	createAndSendLoot.addEventListener('click', (event) => {
+		createAndSendLoot.style.height = '500px';
+		newLootForm.style.display = 'flex';
+		newLootForm.style.visibility = 'visible';
+		newLootForm.style.opacity = '1';
+	});
+	chooseAndSendLoot.addEventListener('click', (event) => {
+		chooseAndSendLoot.style.height = 'fit-content';
+		existingLootForm.style.display = 'flex';
+		existingLootForm.style.visibility = 'visible';
+		existingLootForm.style.opacity = '1';
+	});
+}
